99daabddfae156f0053360f7c7bb043763209742
Farizpurnomo <farizpurnomo91@gmail.com>
2020-05-09 00:34:26 +0700

    list food dan scanner(scanner beserta javanya)

---
+++ Halal_nutrition/app/src/main/java/com/acomp/khobarapp/activity_scan.java
@@ -0,0 +1,53 @@
+package com.acomp.khobarapp;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_scan extends AppCompatActivity {
+
+    ObjectAnimator animator;
+    View scannerLayout;
+    View scannerBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        //Scanner overlay
+        scannerLayout = findViewById(R.id.scannerLayout);
+        scannerBar = findViewById(R.id.scannerBar);
+
+        animator = null;
+
+        ViewTreeObserver vto = scannerLayout.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                scannerLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    scannerLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    scannerLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+
+                float destination = (float)(scannerLayout.getY() + scannerLayout.getHeight());
+                animator = ObjectAnimator.ofFloat(scannerBar, "translationY", scannerLayout.getY(), destination);
+                animator.setRepeatMode(ValueAnimator.REVERSE);
+                animator.setRepeatCount(ValueAnimator.INFINITE);
+                animator.setInterpolator(new AccelerateDecelerateInterpolator());
+                animator.setDuration(3000);
+                animator.start();
+            }
+        });
+
+
+    }
+}
